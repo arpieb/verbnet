a989c9f0993000ef45686ffad710b4d7a95c0b15
arpieb <rob@octobang.com>
2017-01-15 18:01:15 -0500

    Project creation

---
+++ lib/verbnet.ex
@@ -0,0 +1,18 @@
+defmodule VerbNet do
+  @moduledoc """
+  Documentation for VerbNet.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> VerbNet.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
