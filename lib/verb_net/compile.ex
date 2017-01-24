f26eec1bbb5a67a4367db2bf18aa1fb9cb418a1d
arpieb <rob@octobang.com>
2017-01-24 16:26:26 -0500

    Added class members lookup

--- lib/verb_net/compile.ex
+++ lib/verb_net/compile.ex
@@ -11,7 +11,7 @@ defmodule VerbNet.Compile do
   end
 
   @doc ~S"""
-  Convert {key, value} tuples into maps, als convert bitstrings to binaries.
+  Convert {key, value} tuples into maps, also convert bitstrings to binaries.
   """
   def attrs_to_map(attrs) do
     Enum.reduce(attrs, %{}, fn({key, val}, acc) -> Map.put(acc, bitstring_to_atom(key), to_string(val)) end)
@@ -22,13 +22,16 @@ defmodule VerbNet.Compile do
   """
   def content_to_list(content_list) do
     for item <- content_list, into: [] do
-      case item do
-        {tag, attr, content} ->
-            simpleform_to_map(item)
-        _ ->
-            to_string(item)
-      end
+      content_to_list_item(item)
     end
+  end
+
+  defp content_to_list_item({_tag, _attr, _content} = item) do
+    simpleform_to_map(item)
+  end
+
+  defp content_to_list_item(item) do
+    to_string(item)
   end
 
   @doc ~S"""
@@ -39,5 +42,23 @@ defmodule VerbNet.Compile do
     |> to_string()
     |> String.downcase()
     |> String.to_atom()
+  end
+
+  @doc ~S"""
+  Extract sections for VerbNet class into a map, keyed by section.
+  """
+  def extract_sections(classdef) do
+    classdef
+    |> Enum.map(&extract_section/1)
+    |> Map.new()
+  end
+
+  defp extract_section({:members, _attrs, members}) do
+    new_members = Enum.map(members, fn({:member, attrs, _rest}) -> {Map.get(attrs, :name), Map.delete(attrs, :name)} end)
+    {:members, Map.new(new_members)}
+  end
+
+  defp extract_section({type, _attrs, _rest}) do
+    {type, %{}}
   end
 end
--- lib/verbnet.ex
+++ lib/verbnet.ex
@@ -14,10 +14,23 @@ defmodule VerbNet do
     {:ok, vn_class, _rest} = File.read!(fname) |> :erlsom.simple_form()
 
     # Postprocess the erlsom tuple into values we can unquote.
-    {:vnclass, %{id: vnclid}, classdef} = VerbNet.Compile.simpleform_to_map(vn_class)
+    {:vnclass, %{id: class_id}, classdef} = VerbNet.Compile.simpleform_to_map(vn_class)
     classdef_esc = Macro.escape(classdef)
 
+    sections = VerbNet.Compile.extract_sections(classdef)
+    members_esc = sections |> Map.get(:members, %{}) |> Macro.escape()
+
     # Define lookup functions for this VerbNet class.
-    def class(unquote(vnclid)), do: unquote(classdef_esc)
+    def class(unquote(class_id)), do: unquote(classdef_esc)
+    def members(unquote(class_id)), do: unquote(members_esc)
   end
+
+  def class(_) do
+    []
+  end
+
+  def members(_) do
+    []
+  end
+
 end
