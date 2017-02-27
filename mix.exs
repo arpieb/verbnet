ea9691dd0b51c309d0c4482c3a7b2aa5e25ce940
arpieb <rob@octobang.com>
2017-02-27 16:14:43 -0500

    Added missing assets folder to Hex package

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule VerbNet.Mixfile do
   def project do
     [
       app: :verbnet,
-      version: "0.2.1",
+      version: "0.2.2",
       elixir: "~> 1.4",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
@@ -58,6 +58,7 @@ defmodule VerbNet.Mixfile do
   defp package do
     [
       files: [
+        "assets",
         "lib",
         "mix.exs",
         "README*",
