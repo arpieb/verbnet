47728f54d9de527b67e34356dd2ae71b4c588d2a
arpieb <rob@octobang.com>
2017-03-22 14:41:23 -0400

    Updated deps, README for release

--- mix.exs
+++ mix.exs
@@ -44,7 +44,7 @@ defmodule VerbNet.Mixfile do
   # Type "mix help deps" for more examples and options
   defp deps do
     [
-      {:ex_doc, "~> 0.14.5", only: :dev},
+      {:ex_doc, "~> 0.15.0", only: :dev},
       {:erlsom, "~> 1.4"},
     ]
   end
